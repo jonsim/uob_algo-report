d7a6123cdf74b73bed8da4a7f0c034834ee06020
Rupert Bedford <rupertpkb@gmail.com>
2012-12-06 10:27:41 +0000

    added data files, updated the stats thing and added the the results to the report

--- graphs_and_stats/statistical_tests.py
+++ graphs_and_stats/statistical_tests.py
@@ -4,8 +4,12 @@ from scipy.stats import wilcoxon
 from scipy.stats import ttest_rel
 from scipy.stats import ranksums
 import matplotlib.pyplot as plot
+import sys
 
-f = open("run2.csv", "r")
+input_filename = "run2.csv)"
+if len(sys.argv) > 1:
+    input_filename = sys.argv[1]
+f = open(input_filename, "r")
 
 results = []
 
@@ -18,9 +22,13 @@ for line in f:
 		except:
 			results.append([col])
 
+for i, result in zip(range(len(results)), results):
+    print i, sum(result) / len(result)
+
 print "---------- RESULTS ----------"
 print "Normal Test", normaltest(results[1])
 print "Mann-Whitney", mannwhitneyu(results[1], results[3])
 print "Wilcoxon", wilcoxon(results[1], results[3])
 print "T-Test", ttest_rel(results[1], results[3])
-print "Rank Sums", ranksums(results[1], results[3])
+
+print "Rank Sums", ranksums(results[1], results[9])
